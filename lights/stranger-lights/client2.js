8071ae2ee8742c3c2cc10bc7e0df7a9758fdb610
Nathan O'Hanlon <nathan@eit.co.nz>
2021-10-02 13:30:59 +1300

    Latest

--- lights/stranger-lights/client2.js
+++ lights/stranger-lights/client2.js
@@ -25,7 +25,34 @@ neopixels.render(pixelData);
 var lastMessageTime = 0; 
 var socket;
 var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-var lightIndex = config.lightIndex;
+var lightIndex = {
+	A : 42, 
+	B : 43, 
+	C : 44, 
+	D : 45, 
+	E : 46, 
+	F : 47, 
+	G : 48, 
+	H : 49, 
+	I : 40, 
+	J : 39, 
+	K : 38, 
+	L : 37, 
+	M : 36, 
+	N : 35, 
+	O : 34, 
+	P : 33, 
+	Q : 32, 
+	R : 22,
+	S : 23,
+	T : 24,
+	U : 25,
+	V : 26,
+	W : 27,
+	X : 28,
+	Y : 29,
+	Z : 30
+};
 
 var lightColoursByIndex = []; 
 
