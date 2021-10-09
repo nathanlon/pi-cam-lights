8382b461d2644f19bfb6821842a3306a5ab288c0
Nathan O'Hanlon <nathan@eit.co.nz>
2021-10-09 18:48:30 +1300

    Lateset

---
+++ cam/motion_test.py
@@ -0,0 +1,47 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import datetime
+import imutils
+import time
+import cv2
+import json
+import eventlet
+from eventlet import wsgi, websocket
+
+print("done warming up test")
+
+@websocket.WebSocketWSGI
+def greeting_handle(ws):
+	firstFrame = None
+	print("Connected Test")
+
+	time.sleep(2.0)
+
+	startLight = 1
+	widthLights = 1
+
+	while True:
+		startLight = startLight + 1
+		widthLights = widthLights + 1
+		print("x: " + str(startLight) + ", w: " + str(widthLights))
+		ws.send(json.dumps({ 'greeting': {'s': startLight, 'w': str(widthLights) }}))
+		time.sleep(0.1)
+		if startLight > lightCount:
+			startLight = 1
+			widthLights = 1
+
+
+def site(env, start_response):
+    if env['PATH_INFO'] == '/greeting':
+        return greeting_handle(env, start_response)
+    else:
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return ['Eventlet running...']
+
+
+def run_start():
+	listener = eventlet.listen(('0.0.0.0', 8000))
+	wsgi.server(listener, site)
+
+if __name__ == '__main__':
+    run_start()
